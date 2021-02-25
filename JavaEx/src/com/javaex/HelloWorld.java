0f0753162a2b8d2d46229827e3e0ea9292da014a
남승균 <skyun..nam@gmail.com>
2021-02-25 16:45:03 +0900

    Initial Commit

---
+++ JavaEx/src/com/javaex/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+//	주석: 컴파일되지 않는 설명문
+public class HelloWorld {
+	/*
+	 * 여러 줄 주석
+	 */
+	//	객체가 실행하고자 한다면 main 메서드가 필요
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
