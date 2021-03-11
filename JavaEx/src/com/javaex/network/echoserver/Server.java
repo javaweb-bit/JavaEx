1530f77db8787d8d63074c24ccd090ee07bcd806
남승균 <skyun.nam@gmail.com>
2021-03-11 10:48:22 +0900

    Echo Server v2

--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -1,6 +1,10 @@
 package com.javaex.network.echoserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,6 +33,30 @@ public class Server {
 			System.out.println("SERVER: [클라이언트가 연결되었습니다]");
 			System.out.println("	클라이언트:" + socketAddress.getAddress() 
 								+ ":" + socketAddress.getPort());
+			
+			
+			
+			//	메시지 수신
+			InputStream is = socket.getInputStream();
+			Reader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			//	메시지 읽어오기
+			String message;
+			
+			while(true) {
+				message = br.readLine();
+				
+				if (message == null) {	//	읽을 메시지 없음
+					System.out.println("SERVER: 접속을 종료합니다.");
+					break;
+				}
+				
+				System.out.println("SERVER: [수신 메시지]" + message);
+			}
+			
+			
+			
 			
 			//	후처리
 			System.out.println("SERVER: [서버를 종료합니다]");
